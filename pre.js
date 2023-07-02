10da6dd2aacb43924ac84f45cf35e4069b3a07bf
sainkithakur <sainkithakur04@gmail.com>
2023-07-02 14:53:06 +0530

    first commit

---
+++ pre.js
@@ -0,0 +1,163 @@
+//var myAge = "22"
+// Number.NaN = NaN;
+// let x = 2
+// let y = 4
+// console.log(x++y)
+//console.log(typeof(myAge))
+// var num =15;
+// var newNum = --num + 5;
+// console.log(num);
+// console.log(newNum);
+// var tomr = "rain";
+// if(tomr == "rain"){
+//     console.log("take a raincoat");
+
+
+// }else{
+//     console.log("No need to take a raincoat")
+// }
+// var age = 17;
+// console.log((age >= 18 ) ? "you can vote" : "you can't vote");
+
+
+// var year = 2016
+// if(year % 4 ===0){
+//     if(year % 100 === 0){
+//         if(year % 400 === 0){
+//             console.log("The year" + year + "is a leap year");
+//         }else{
+//             console.log("The year" + year + "is not a leap year");
+//         }
+//         }else{console.log("The year" + year + "is a leap year");
+//     }
+//     }else{
+//         console.log("The year" + year + "is not a leap year");
+//     }
+
+// var age = 19;
+// if(age >= 18){
+//     console.log("you are eligible to vote");
+// }else{
+//     console.log("you ara not eligible to vote");
+// }
+    
+
+// var area = "circle";
+// var PI = 3.142, l=5, b=4, r=3;
+// if(area == "circle"){
+//     console.log("the of the circlr is : " + PI*r**2);
+
+// }else if(area =="triangle"){
+//     console.log("the area of the triangle is :" + (l*b));
+// }else if (area == "rectangle"){
+//     console.log("the area of the rectangle is : " + (l*b));
+// }
+
+// var area = "circle";
+// var PI = 3.142, l=5, b=4, r=3;
+// switch(area){
+//     case "circle":
+//     console.log("the of the circlr is : " + PI*r**2);
+// case 'rectangle':
+//     console.log("the area of the rectangle is : " + (l*b)/2);
+//     case 'triangle':
+//         console.log("the area of the triangle is :" + (l*b));
+// }
+
+
+
+// var num=0;
+// while(num <= 1000){
+//     console.log(num);
+//     num++;
+// }
+
+
+//  var num=0;
+//  do{
+//      console.log(num);
+//      num++;
+//     }
+//     while(num <= 10);
+
+
+// for(var num = 1; num <= 10; num++){
+//     var tableof = 8;
+//     console.log(tableof + " * " + num + " = " + tableof * num);
+// }
+
+
+// function sum(a,b) {
+ 
+//     var total = a+b;
+
+// console.log( total );
+// }    
+
+//  sum(); 
+//  sum(20,30)
+//  sum(50,50)
+
+
+//   function sum(a,b) {
+ 
+//     var total = a+b;
+
+//  console.log( total );
+//  }    
+// var funExp = sum(5,15);
+
+
+//  var funExp = function (a,b) {
+ 
+//      return total = a+b;
+//   }    
+//  var sum = funExp(15,15);
+// console.log('the sun of teo no is' + sum );
+
+
+// for(let num = 1; num<= 10; num++){
+//     let tableof = 12;
+//     console.log( ` ${tableof} * ${num} = ${tableof * num}` );
+// }
+
+// function mult(a,b=5){
+//     return a*b;
+// }
+// console.log(mult(5));
+
+// console.log(sum());
+// function sum() {
+//     let a = 5; b = 6;
+//     let sum = a+b;
+//     return `the sun of  the two number is ${sum}`
+// }
+
+
+// Array
+// var myFriend = ['ram','raman','arjun','vishal'];
+// console.log(myFriend[myFriend.length-1]);
+
+
+
+// var myFriends = ['ram','raman','arjun','vishal'];
+// for(var i=0; i<myFriends.length; i++){
+//     console.log(myFriends[i]);
+// }
+
+
+// var myFriends = ['ram','raman','arjun','vishal'];
+// for(let elements in myFriends){
+//     console.log(elements);
+// }
+
+
+// var myFriends = ['ram','raman','arjun','vishal'];
+// for(let elements of myFriends){
+//     console.log(elements);
+// }
+
+// var myFriends = ['ram','raman','arjun','vishal'];
+// myFriends.forEach(function(element, index, array) {
+//     console.log(element);
+// })
---
+++ spread.js
@@ -0,0 +1,86 @@
+// Spread operator
+// arr = [1, 2, 4, 6, 32]
+
+// function avg(a,b,c){
+//     return a+b+c/3
+// }
+
+// //let a = avg(...arr)
+// let a =[4,7, ...arr, 67]
+// console.log(a)
+
+
+// let obj1 = {
+//     name: "Harry",
+//     class: "5b",
+//     favLang: "Javascript"
+// }
+// let obj2 = {}
+// obj2.name = obj1.name;
+// console.log(obj1)
+
+// let obj1 = {
+//     name1: "Harry",
+//     class1: "5b",
+//     favLang: "Javascript"
+// }
+// let name1=obj1.name1;
+// let class1 = obj1.class1;
+// let favLang = obj1.favLang;
+
+// console.log(name1, class1, favLang)
+
+
+// console.log("Harry is a good boy")
+
+// setTimeout(() =>{
+// console.log("Harry is inside settimeout")
+// }, 1000);
+
+// console.log("Harry is a bad boy")
+
+
+
+//promises  arrys
+
+
+// let fs = require("fs/promises");
+// fs.readFile("file.txt", "utf-8");
+// a.then((data)=>{
+//     console.log(err, data);
+// })
+
+// console.log("This is eof")
+
+
+// let fs = require("fs/promises");
+
+// const readThree = (file1, file2, file3)=>{
+//     let a1 = fs.readFile("file.txt", "utf-8");
+//     let a2 = fs.readFile("file.txt", "utf-8");
+//     let a3 = fs.readFile("file.txt", "utf-8");
+//     let c1 = await a1;
+//     console.log(c1)
+//     let c2 = await a2;
+//     console.log(c2)
+//     let c3 = await a3;
+//     console.log(c3)
+//}
+
+
+// let a = [1, 2, 3, 4, 5]
+// let a2 = a.map((value)=>{
+//     return value+ 6;
+// })z
+// console.log(a, a2)
+
+// let a = fs.readFile("file.txt", "utf-8");
+// a.then((data)=>{
+//     console.log(data);
+// })
+
+// console.log("This is eof")
+
+
+//filter
+
